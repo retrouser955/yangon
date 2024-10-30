e251af37ffa7bdefdd69867be7035d4e1d6c0d67
Retro <70205403+retrouser955@users.noreply.github.com>
2024-10-30 18:51:33 +0630

    add more description

--- example/commands/say.ts
+++ example/commands/say.ts
@@ -4,6 +4,9 @@ import { commandMetadataOptionString, command } from "../../lib";
 export default class SayCommand {
     @command("Repeat something from the bot")
     async say(ctx: ChatInputCommandInteraction) {
+        // No need to create an option in SlashCommandBuilder
+        // Discord Chakra will automatically recognize the option and will transpile it into a SlashCommandOptionString
+        // On every run, the function will also get the user input so no need to use `interaction.option.getString()` anymore
         const content = commandMetadataOptionString({
             required: true,
             name: "content",
